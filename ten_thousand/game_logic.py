64136876bf20da18ca5dd91762606e1fe372e454
Brentice Loper <brentice@icloud.com>
2022-07-26 00:15:19 -0700

    added test.

---
+++ ten_thousand/game_logic.py
@@ -0,0 +1,21 @@
+from collections import Counter
+import random
+
+class GameLogic:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def calculate_score(roll):
+        banked_score = tuple(roll)
+        if banked_score == (1,):
+            return 100
+        elif banked_score == (1,1):
+            return 200
+
+    @staticmethod
+    def roll_dice(dice):
+        roll = []
+        for i in range(dice):
+            roll.append(random.randint(0,6))
+            return tuple(roll)
