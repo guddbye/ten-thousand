5119d13f4a88caa2ca833470a2b0eff2280b1978
brentice <brentice@icloud.com>
2022-07-31 15:56:59 -0700

    logic and game.py complete. editing to make everything work correctly.

---
+++ ten_thousand/game.py
@@ -0,0 +1,167 @@
+from game_logic import GameLogic, Banker
+
+def play(roller=GameLogic.roll_dice, num_rounds=20):
+    choice = ask_to_play()
+    if choice == "y":
+        start_game(num_rounds)
+    else:
+        decline_game()
+
+def ask_to_play():
+    print("Welcome to Ten Thousand")
+    print("(y)es to play or (n)o to decline")
+    return input("> ")
+
+def start_game(num_rounds):
+    # track rounds played
+    round_count = 1
+    # run game for given number of rounds
+    while round_count <= num_rounds:
+        round_result = do_round(round_count)
+        if round_result == "q":
+            break
+
+        round_count += 1
+
+    print(f"Thanks for playing. You earned {Banker.get_score()} points!")
+
+def do_round(round_count):
+    start_round(round_count)
+    # track points for this round
+    round_points = 0
+    # create clean starting point for new round
+    num_dice = 6
+    # start the player turn action loop for this round
+    turn_results = play_turn(num_dice)
+    # add turn points to the total points for this round
+    round_points += turn_results
+
+    if turn_results == -1:
+        return "q"
+
+    print(f"You banked {round_points} points in round {round_count}")
+    Banker.add_to_score(round_points)
+    print(f"Total score is {Banker.get_score()} points")
+
+    return round_points
+
+def play_turn(num):
+    num_dice = num
+    keepers = []
+    turn_points = 0
+    while True:
+        # roll remaining dice
+        roll = roll_dice(num_dice)
+        # display the results of the roll
+        display_roll(roll)
+
+        # check if roll is zilch
+        if GameLogic.calculate_score(roll) == 0:
+            zilch()
+            return 0
+
+        # prompt user to keep dice, verify that input is valid
+        while True:
+            print("Enter dice to keep, or (q)uit:")
+            keep = input("> ")
+            # check if user quit
+            if keep == "q":
+                return -1
+
+            # check if they're a dirty cheater
+            if cheater(roll, keep) is True:
+                print("Cheater!!! Or possibly made a typo...")
+                display_roll(roll)
+
+            if cheater(roll, keep) is False:
+                break
+
+        # keep selected dice
+        new = GameLogic.dice_shelf(keep)
+        for num in new:
+            keepers.append(num)
+        # update number of remaining dice
+        num_dice = 6 - len(keepers)
+
+        # score kept dice
+        points = GameLogic.calculate_score(new)
+
+        # hot dice - if all dice score, refresh dice
+        if num_dice == 0:
+            turn_points += points
+            keepers = []
+            num_dice = 6
+
+        if num_dice < 6:
+            # update points earned this turn
+            turn_points += points
+
+        print(f"You have {turn_points} unbanked points and {num_dice} dice remaining")
+        print("(r)oll again, (b)ank your points or (q)uit:")
+        choice = input("> ")
+        if choice == "q":
+            return -1
+
+        if choice == "b":
+            return turn_points
+
+def cheater(roll, keep):
+    cheating = True
+    roll_list = list(roll)
+    keep_list = list(keep)
+    check_vals = [int(num) for num in keep_list]
+    val_count = 0
+    for val in check_vals:
+        roll_count = 0
+        print(f"roll list: {roll_list}")
+        print(f"check vals: {check_vals}")
+        print(f"val count: {val_count}, roll count: {roll_count}")
+        for num in roll_list:
+            print(f"roll count: {roll_count}")
+            print(f"num: {num}, val: {val}")
+            if num == val:
+                print(f"roll list pop: {roll_list[roll_count]}")
+                print(f"check val pop: {check_vals[val_count]}")
+                roll_list.pop(roll_count)
+                check_vals.pop(val_count)
+                break
+            roll_count += 1
+        # val_count += 1
+
+    if len(check_vals) == 0:
+        cheating = False
+    return cheating
+
+def roll_dice(num_dice):
+    print(f"Rolling {num_dice} dice...")
+    return GameLogic.roll_dice(num_dice)
+
+def add_to_shelf(keep):
+    return GameLogic.dice_shelf(keep)
+
+def display_roll(roll):
+    print(f"*** {format_roll(roll)} ***")
+
+def format_roll(roll):
+    roll_values = []
+    for val in roll:
+        roll_values.append(str(val))
+    formatted_roll = " ".join(roll_values)
+    return formatted_roll
+
+def start_round(round_count):
+    print(f"Starting round {round_count}")
+
+def zilch():
+    print("****************************************")
+    print("**        Zilch!!! Round over         **")
+    print("****************************************")
+
+def decline_game():
+    print("OK. Maybe another time")
+
+def quit_game():
+    print(f"Thanks for playing. You earned {Banker.get_score()} points")
+
+if __name__ == '__main__':
+    play()
--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -1,289 +1,151 @@
-from random import randint
+import random
 from collections import Counter
 
+class Banker:
+    score = 0
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def add_to_score(points):
+        Banker.score = Banker.score + points
+
+    @staticmethod
+    def get_score():
+        return Banker.score
 
 class GameLogic:
     def __init__(self):
-        self.head = None
+        pass
 
     @staticmethod
-    def calculate_score(dice_roll):
-        # dice_roll is a tuple
-        score = 0
-        roll = Counter(dice_roll).most_common()
+    def dice_shelf(num_str):
+        dice = list(num_str)
+        return tuple(dice)
 
-        if len(dice_roll) == 0:
+    @staticmethod
+    def roll_dice(num):
+        rolls = []
+        while len(rolls) < num:
+            rolls.append(random.randint(1, 6))
+        return tuple(rolls)
+
+    @staticmethod
+    def calculate_score(tup):
+        score = 0
+        str_list = list(tup)
+        nums = [int(num) for num in str_list]
+        roll = Counter(nums).most_common()
+
+        if len(tup) == 0:
             return score
 
         # straight
         if len(roll) == 6:
             return 1500
 
+        #6 of a kind
+        if roll[0][1] == 6:
+            if roll[0][0] == 1:
+                return 4000
+            return int(roll[0][0]) * 400
+
+        #5 of a kind
+        if roll[0][1] == 5:
+            if len(roll) == 2:
+                if roll[1][0] == 5:
+                    score += 50
+
+                if roll[1][0] == 1:
+                    score += 100
+
+            if roll[0][0] == 1:
+                return score + 3000
+
+            score += int(roll[0][0]) * 300
+            return score
+
+        #4 of a kind
+        if roll[0][1] == 4:
+            if len(roll) >= 2:
+                if roll[1][0] == 5:
+                    score += 50 * roll[1][1]
+
+                if roll[1][0] == 1:
+                    score += 100 * roll[1][1]
+
+            if len(roll) == 3:
+                if roll[2][0] == 5:
+                    score += 50 * roll[2][1]
+
+                if roll[2][0] == 1:
+                    score += 100 * roll[2][1]
+
+            if roll[0][0] == 1:
+                return score + 2000
+
+            score += int(roll[0][0]) * 200
+            return score
+
+        # double 3 of a kind
+        if len(roll) == 2:
+            if roll[0][1] == 3 and roll[1][1] == 3:
+                if roll[0][0] == 1:
+                    score += 1000
+                    return score + roll[1][0] * 100
+
+                if roll[1][0] == 1:
+                    score += 1000
+                    return score + roll[0][0] * 100
+
+                return roll[0][0] * 100 + roll[1][0] * 100
+
+        # 3 of a kind
+        if roll[0][1] == 3:
+            if len(roll) >= 2:
+                if roll[1][0] == 5:
+                    score += 50 * roll[1][1]
+
+                if roll[1][0] == 1:
+                    score += 100 * roll[1][1]
+
+            if len(roll) >= 3:
+                if roll[2][0] == 5:
+                    score += 50 * roll[2][1]
+
+                if roll[2][0] == 1:
+                    score += 100 * roll[2][1]
+
+            if len(roll) == 4:
+                if roll[3][0] == 5:
+                    score += 50 * roll[3][1]
+
+                if roll[3][0] == 1:
+                    score += 100 * roll[3][1]
+
+            if roll[0][0] == 1:
+                return score + 1000
+
+            score += int(roll[0][0]) * 100
+            return score
+
         # 3 pairs
         if len(roll) > 2:
-            if roll[0][1] == 2 and roll[1][1] == 2 and roll[2][1] == 2:
+            if roll[0][1] and roll[1][1] and roll[2][1] == 2:
                 score += 1500
                 return score
 
-        # 2 sets of 3
-        if len(roll) == 2:
-            if roll[0][1] == 3 and roll[1][1] == 3:
-                score += 1200
-                return score
+        # handle single 1's and 5's
+        one_counter = 0
+        five_counter = 0
+        for num in nums:
+            if num == 1:
+                one_counter += 1
+            if num == 5:
+                five_counter += 1
+        score += one_counter * 100
+        score += five_counter * 50
 
-        for num in roll:
-            # Score cases for 1
-            if num[0] == 1:
-                if num[1] == 1:
-                    score += 100
-                if num[1] == 2:
-                    score += 200
-                if num[1] == 3:
-                    score += 1000
-                if num[1] == 4:
-                    score += 2000
-                if num[1] == 5:
-                    score += 3000
-                if num[1] == 6:
-                    score += 4000
-
-            # Score cases for 2
-            if num[0] == 2:
-                if num[1] < 3:
-                    score += 0
-                if num[1] == 3:
-                    score += 200
-                if num[1] == 4:
-                    score += 400
-                if num[1] == 5:
-                    score += 600
-                if num[1] == 6:
-                    score += 800
-
-            # Score cases for 3
-            if num[0] == 3:
-                if num[1] < 3:
-                    score += 0
-                if num[1] == 3:
-                    score += 300
-                if num[1] == 4:
-                    score += 600
-                if num[1] == 5:
-                    score += 900
-                if num[1] == 6:
-                    score += 1200
-
-            # Score cases for 4
-            if num[0] == 4:
-                if num[1] < 3:
-                    score += 0
-                if num[1] == 3:
-                    score += 400
-                if num[1] == 4:
-                    score += 800
-                if num[1] == 5:
-                    score += 1200
-                if num[1] == 6:
-                    score += 1600
-
-            # Score cases for 5
-            if num[0] == 5:
-                if num[1] == 1:
-                    score += 50
-                if num[1] == 2:
-                    score += 100
-                if num[1] == 3:
-                    score += 500
-                if num[1] == 4:
-                    score += 1000
-                if num[1] == 5:
-                    score += 1500
-                if num[1] == 6:
-                    score += 2000
-
-            # Score cases for 6
-            if num[0] == 6:
-                if num[1] < 3:
-                    score += 0
-                if num[1] == 3:
-                    score += 600
-                if num[1] == 4:
-                    score += 1200
-                if num[1] == 5:
-                    score += 1800
-                if num[1] == 6:
-                    score += 2400
-
+        # default/catch all return
         return score
-
-    @staticmethod
-    def roll_dice(num_dice):
-        results = []
-        for num in range(num_dice):
-            roll = randint(1, 6)
-            results.append(roll)
-        return tuple(results)
-
-    @staticmethod
-    def validate_keepers(roll, keepers):
-        roll_count = Counter(sorted(roll)).most_common()
-        keepers_count = Counter(sorted(keepers)).most_common()
-
-        r_qty = []
-        k_qty = []
-        for num in roll_count:
-            r_qty.append(num[1])
-        for bum in keepers_count:
-            k_qty.append(bum[1])
-        if set(sorted(keepers)).issubset(sorted(roll)) and sorted(k_qty) <= sorted(r_qty):
-            return True
-        return False
-
-    @staticmethod
-    def get_scorers(tup):
-        scorers = ()
-        losers = ()
-        index = 0
-        for num in tup:
-            index += 1
-
-        # empty case
-        if tup == ():
-            return scorers
-
-        # Check first value from input
-        if index >= 1:
-            one = tup[0],
-            if GameLogic.calculate_score(one) > 0:
-                scorers += one
-            else:
-                losers += one
-
-        # Check second value from input
-        if index >= 2:
-            two = tup[1],
-            if GameLogic.calculate_score(two) > 0:
-                scorers += two
-            else:
-                losers += two
-        else:
-            return scorers
-
-        # Check third value from input
-        if index >= 3:
-            three = tup[2],
-            if GameLogic.calculate_score(three) > 0:
-                scorers += three
-            else:
-                losers += three
-
-        # Check fourth value from input
-        if index >= 4:
-            four = tup[3],
-            if GameLogic.calculate_score(four) > 0:
-                scorers += four
-            else:
-                losers += four
-
-        # Check fifth value from input
-        if index >= 5:
-            five = tup[4],
-            if GameLogic.calculate_score(five) > 0:
-                scorers += five
-            else:
-                losers += five
-
-        # Check six value from input
-        if index >= 6:
-            six = tup[5],
-            if GameLogic.calculate_score(six) > 0:
-                scorers += six
-            else:
-                losers += six
-
-        print(losers)
-        return tuple(sorted(scorers))
-
-    def play_dice(self):
-        print("Welcome to Ten Thousand!")
-        while True:
-            score_points = 0
-            print("(y)es to play or (n)o to decline.")
-            choice = input("> ")
-            round_num = 1
-            if choice == "n":
-                print("OK. Maybe another time.")
-                break
-            elif choice == "y":
-                while True:
-                    print(f"Starting round {round_num}")
-                    num_di = 6
-                    roll_values = []
-                    roll = GameLogic.roll_dice(num_di)
-                    roll_length = len(roll)
-                    print(f"Rolling {roll_length} dice...")
-                    for value in roll:
-                        roll_values.append(str(value))
-
-                    formatted_roll = " ".join(roll_values)
-                    print(f"*** {formatted_roll} ***")
-
-                    print("Enter dice to keep, or (q)uit:")
-                    choice = input("> ")
-
-                    if choice == "q":
-                        print(f"Thanks for playing. You earned {score_points} points")
-                        break
-
-                    else:
-                        keepers = [int(d) for d in str(choice)]
-                        # TODO: Why is line 243 - 254 being skipped????
-                        # attempting to validate cheat_and_fix.sim.txt
-                        test = GameLogic.validate_keepers(tuple(roll), tuple(keepers))
-                        print(test)
-                        if test == True:
-                            continue
-                        if test == False:
-                            while test == False:
-                                print("Cheater!!! Or possibly made a typo...")
-                                print(f"*** {formatted_roll} ***")
-                                print("Enter dice to keep, or (q)uit:")
-                                choice = input("> ")
-                                if test == True:
-                                    break
-                                break
-
-                        points_potential = [int(d) for d in str(choice)]
-
-                        for point in points_potential:
-                            num_di -= 1
-                        points = GameLogic.calculate_score(tuple(points_potential))
-                        print(f"You have {points} unbanked points and {num_di} dice remaining")
-                        while True:
-
-                            print("(r)oll again, (b)ank your points or (q)uit:")
-                            choice = input("> ")
-                            if choice == "b":
-                                score_points += points
-                                print(f"You banked {points} in round {round_num}")
-                                print(f"Total score is {score_points} points")
-                                round_num += 1
-                                break
-                            elif choice == "q":
-                                break
-                            break
-
-                break
-
-
-if __name__ == "__main__":
-
-    test2 = GameLogic.roll_dice(6)
-
-    test3 = GameLogic.play_dice(test2)
-    # test4 = GameLogic.get_scorers((3,3,3,1,1))
-    # print(test4)
-    # roll = (5,5,1,4,4,2)
-    # points = [int(d) for d in str(555)]
-    # test5 = GameLogic.validate_keepers(roll, tuple(points))
-    # print(test5)
